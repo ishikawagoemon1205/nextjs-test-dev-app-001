3f2273af42d88586f920a796465dabdcc32c0ee1
atsuhiro <ishikawaatsuhito1205@gmail.com>
2024-11-19 11:22:17 +0900

    パズルゲーム実装001

--- src/app/practice/slide-puzzle/page.tsx
+++ src/app/practice/slide-puzzle/page.tsx
@@ -4,43 +4,43 @@
 
 export default function SlidePuzzlePage() {
 
-    // const [contentStatus, setContentStatus] = useState("start");
-    const contentStatus = "standby";
-    // const [tiles, setTiles] = useState([]);
-    const hist = [];
+    // // const [contentStatus, setContentStatus] = useState("start");
+    // const contentStatus = "standby";
+    // // const [tiles, setTiles] = useState([]);
+    // const hist = [];
 
-    // useEffect(() => {
-    //     if (contentStatus === "start") {
-    //         // init();
-    //     }
-    // }, [contentStatus]);
+    // // useEffect(() => {
+    // //     if (contentStatus === "start") {
+    // //         // init();
+    // //     }
+    // // }, [contentStatus]);
 
-    const standbyDisplay = () => (
-        <div className="w-[500px] h-[500px] flex items-center justify-center">
-            <button
-                className="bg-[#ffffff] hover:bg-gray-300 w-[200px] h-[30px] rounded-[4px]"
-                // onClick={() => setContentStatus("start")}
-            >
-                <h4 className="text-[#000000] font-bold">スタート</h4>
-            </button>
-        </div>
-    );
+    // const standbyDisplay = () => (
+    //     <div className="w-[500px] h-[500px] flex items-center justify-center">
+    //         <button
+    //             className="bg-[#ffffff] hover:bg-gray-300 w-[200px] h-[30px] rounded-[4px]"
+    //             // onClick={() => setContentStatus("start")}
+    //         >
+    //             <h4 className="text-[#000000] font-bold">スタート</h4>
+    //         </button>
+    //     </div>
+    // );
 
-    const startDisplay = () => (
-        <div className="w-[500px] h-[500px] flex items-center justify-center">
-            <table id="table" />
-        </div>
-    );
+    // const startDisplay = () => (
+    //     <div className="w-[500px] h-[500px] flex items-center justify-center">
+    //         <table id="table" />
+    //     </div>
+    // );
 
     
-    const content = () => {
-        if (contentStatus === "standby") {
-            return standbyDisplay();
-        } else if (contentStatus === "start") {
-            return startDisplay();
-        }
-        return null; // 必要に応じてエラー処理
-    };
+    // const content = () => {
+    //     if (contentStatus === "standby") {
+    //         return standbyDisplay();
+    //     } else if (contentStatus === "start") {
+    //         return startDisplay();
+    //     }
+    //     return null; // 必要に応じてエラー処理
+    // };
     
     return (
         <div className="w-[100%] h-[100%] flex items-center justify-center">
@@ -49,7 +49,7 @@ export default function SlidePuzzlePage() {
                     <h4 className="text-[20px] text-[#ffffff]">Slide Puzzle</h4>
                 </div>
                 <div className="w-[100%] h-[500px] flex items-center justify-center">
-                    {content()}
+                    {/* {content()} */}
                 </div>
             </div>
         </div>
